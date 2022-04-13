8955207bfcc8a8d79d13e42dfc11136dced04aee
sdaksfjcnakjdb <82866450+sdaksfjcnakjdb@users.noreply.github.com>
2022-04-13 18:23:13 +0800

    修改前端，增加下拉框标签自定义功能。并且修改图谱增加功能为不需导入文件也可以进行图谱新增。

--- neo4j/neo4j/src/main/java/com/sunsheen/neo4j_demo1/dao/DatabaseDao.java
+++ neo4j/neo4j/src/main/java/com/sunsheen/neo4j_demo1/dao/DatabaseDao.java
@@ -6,7 +6,8 @@ import org.neo4j.driver.v1.GraphDatabase;
 
 public class DatabaseDao implements AutoCloseable{
     public static String uurl = "555";
-    public static Driver driver = GraphDatabase.driver( "bolt://172.18.194.215:7687", AuthTokens.basic( "neo4j", "neo4j" ) );
+//    public static Driver driver = GraphDatabase.driver( "bolt://172.18.194.215:7687", AuthTokens.basic( "neo4j", "neo4j" ) );
+    public static Driver driver = GraphDatabase.driver( "bolt://localhost:7687", AuthTokens.basic( "neo4j", "neo4j" ) );
 //    public static Driver driver = GraphDatabase.driver( "bolt://localhost:7687", AuthTokens.basic( "neo4j", "neo4j" ) );
 
     public static void con(String url, String name, String password){
